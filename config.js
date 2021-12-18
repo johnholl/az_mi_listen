76cd2449f3a7ddab072a93a947631378b8dd0c8e
John Holler <johnholl@umich.edu>
2021-12-18 14:36:24 -0500

    hide config

--- config.js
+++ config.js
@@ -1,2 +1,10 @@
 export const loc = "AZ";
 export const otherloc = "MI";
+export const firebaseConfig = {
+    apiKey: "AIzaSyCEIXOpsCFvJa9OuezuSIM3ARHNnD12cOM",
+    authDomain: "aztomi-connection.firebaseapp.com",
+    projectId: "aztomi-connection",
+    storageBucket: "aztomi-connection.appspot.com",
+    messagingSenderId: "843850439745",
+    appId: "1:843850439745:web:b062a8f1152163afa1fac0"
+  };
--- firebase.js
+++ firebase.js
@@ -1,18 +1,6 @@
 // Import the functions you need from the SDKs you need
 import firebase from 'firebase';
-
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyCEIXOpsCFvJa9OuezuSIM3ARHNnD12cOM",
-  authDomain: "aztomi-connection.firebaseapp.com",
-  projectId: "aztomi-connection",
-  storageBucket: "aztomi-connection.appspot.com",
-  messagingSenderId: "843850439745",
-  appId: "1:843850439745:web:b062a8f1152163afa1fac0"
-};
+import { firebaseConfig } from './config.js';
 
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
